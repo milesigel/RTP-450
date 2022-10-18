6cabe989dad2d76a8fc5ec686d1c36a182a72531
Miles Sigel <mgs6@rice.edu>
2022-10-17 21:46:17 -0500

    added header

--- src/Project3Exercise2.cpp
+++ src/Project3Exercise2.cpp
@@ -1,3 +1,9 @@
+///////////////////////////////////////
+// COMP/ELEC/MECH 450/550
+// Project 3
+// Authors: Miles Sigel, Alex Prucka, Connor Kelley
+//////////////////////////////////////
+
 #include <iostream>
 #include <fstream>
 #include <cmath>
