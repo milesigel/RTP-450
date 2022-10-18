10f63b4aa296eb82e7ca9f79db0076c98575ad52
Aprucka19 <awp@prucka.com>
2022-10-18 10:11:27 -0500

    Q3 additions

--- src/Project3Exercise3.cpp
+++ src/Project3Exercise3.cpp
@@ -5,12 +5,73 @@
 //////////////////////////////////////
 
 #include <iostream>
+#include <ompl/geometric/planners/est/EST.h>
+#include <ompl/geometric/planners/prm/PRM.h>
+#include <ompl/geometric/planners/rrt/RRT.h>
+#include <ompl/tools/benchmark/Benchmark.h>
+
+
+#include "omplapp/apps/SE3RigidBodyPlanning.h"
+#include "omplapp/config.h"
 
 // Your random tree planner
 #include "RTP.h"
+using namespace ompl;
 
 void benchmarkApartment()
 {
+    app::SE3RigidBodyPlanning setup;
+	std::string benchmark_name;
+	double runtime_limit, memory_limit;
+	int run_count;
+    benchmark_name = std::string("apartment");
+
+	std::string robot_fname = std::string(OMPLAPP_RESOURCE_DIR) + "/3D/Apartment_robot.dae";
+	std::string env_fname = std::string(OMPLAPP_RESOURCE_DIR) + "/3D/Apartment_env.dae";
+	setup.setRobotMesh(robot_fname);
+	setup.setEnvironmentMesh(env_fname);
+
+	base::ScopedState<base::SE3StateSpace> start(setup.getSpaceInformation());
+	start->setX(241.81);
+	start->setY(106.15);
+	start->setZ(70.57);
+	start->rotation().setAxisAngle(0., 0., -1., 3.12413936107);
+
+	base::ScopedState<base::SE3StateSpace> goal(start);
+	goal->setX(-31.19);
+	goal->setY(-99.85);
+	goal->setZ(36.46);
+	goal->rotation().setAxisAngle(0., 0., -1., 3.12413936107);
+
+	setup.setStartAndGoalStates(start, goal);
+	setup.getSpaceInformation()->setStateValidityCheckingResolution(0.01);
+	setup.setup();
+
+	std::vector<double> cs(3);
+	cs[0] = 35;
+	cs[1] = 35;
+	cs[2] = 35;
+	setup.getStateSpace()->getDefaultProjection()->setCellSizes(cs);
+
+	runtime_limit = 10.0;
+	memory_limit = 10000.0;  // set high because memory usage is not always estimated correctly
+	run_count = 50;
+
+
+	tools::Benchmark::Request request(runtime_limit, memory_limit, run_count);
+	tools::Benchmark b(setup, benchmark_name);
+
+	// optionally set pre & pos run events
+//	b.setPreRunEvent([](const base::PlannerPtr &planner) { preRunEvent(planner); });
+//	b.setPostRunEvent(
+//		[](const base::PlannerPtr &planner, tools::Benchmark::RunProperties &run) { postRunEvent(planner, run); });
+
+	b.addPlanner(std::make_shared<geometric::RRT>(setup.getSpaceInformation()));
+	b.addPlanner(std::make_shared<geometric::EST>(setup.getSpaceInformation()));
+	b.addPlanner(std::make_shared<geometric::PRM>(setup.getSpaceInformation()));
+	b.addPlanner(std::make_shared<geometric::RTP>(setup.getSpaceInformation()));
+	b.benchmark(request);
+	b.saveResultsToFile();
     // TODO
 }
 
